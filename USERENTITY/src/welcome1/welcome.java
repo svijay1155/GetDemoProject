3d3a817fc437ddb6e3b9d3b2ed510b44f838dda3
Vijay Selvaraj <vijayvj7094@gmail.com>
2023-10-03 12:24:54 +0530

    this is user entity

---
+++ USERENTITY/src/welcome1/welcome.java
@@ -0,0 +1,16 @@
+package welcome1;
+
+public class welcome {
+
+	public static void main(String[] args) {
+
+		String obj = "WELCOME";
+
+		for (int i = 0; i < obj.length(); i++) {
+			System.out.println(obj.charAt(i));
+
+		}
+		System.out.println("This is vijay");
+	}
+
+}
